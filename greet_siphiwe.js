fda4cf6ea08a31a6275209859e71031e4c8ff2c6
mduduziavatar <59861854+mduduziavatar@users.noreply.github.com>
2020-08-05 11:13:04 +0200

    added ghost font

--- greet_siphiwe.js
+++ greet_siphiwe.js
@@ -4,7 +4,13 @@ const greet = require('./greet');
 
 var figlet = require('figlet');
 
-figlet(greet("Siphiwe!"), function(err, data) {
+figlet.text(greet("Siphiwe!"), {
+    font: 'Ghost',
+    horizontalLayout: 'default',
+    verticalLayout: 'default',
+    width: 80,
+    whitespaceBreak: true
+}, function(err, data) {
     if (err) {
         console.log('Something went wrong...');
         console.dir(err);
